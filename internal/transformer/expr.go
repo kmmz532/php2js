57c29742b7dc11312f89f11beb3cb91371e9575b
kmmz532 <me@kmmz.jp>
2026-07-01 13:15:30 +0900

    feat: implement PHP expression to JS expression transformer logic

---
+++ internal/transformer/expr.go
@@ -0,0 +1,426 @@
+package transformer
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/VKCOM/php-parser/pkg/ast"
+	"github.com/kmmz532/php2js/internal/jsast"
+)
+
+// transformExpr converts a PHP expression to a JS expression.
+func (t *Transformer) transformExpr(node ast.Vertex) jsast.Expression {
+	if node == nil {
+		return &jsast.Literal{Value: "undefined", Kind: "undefined"}
+	}
+
+	switch n := node.(type) {
+	case *ast.ExprVariable:
+		return t.transformVariable(n)
+	case *ast.ScalarString:
+		return &jsast.Literal{Value: string(n.Value), Kind: "string"}
+	case *ast.ScalarLnumber:
+		return &jsast.Literal{Value: string(n.Value), Kind: "number"}
+	case *ast.ScalarDnumber:
+		return &jsast.Literal{Value: string(n.Value), Kind: "number"}
+	case *ast.ScalarEncapsed:
+		return t.transformEncapsed(n)
+	case *ast.ScalarEncapsedStringPart:
+		return &jsast.Literal{Value: fmt.Sprintf(`"%s"`, escapeJSString(string(n.Value))), Kind: "string"}
+	case *ast.ExprAssign:
+		return t.transformAssign(n)
+	case *ast.ExprAssignPlus:
+		return &jsast.AssignExpr{Op: "+=", Left: t.transformExpr(n.Var), Right: t.transformExpr(n.Expr)}
+	case *ast.ExprAssignMinus:
+		return &jsast.AssignExpr{Op: "-=", Left: t.transformExpr(n.Var), Right: t.transformExpr(n.Expr)}
+	case *ast.ExprAssignMul:
+		return &jsast.AssignExpr{Op: "*=", Left: t.transformExpr(n.Var), Right: t.transformExpr(n.Expr)}
+	case *ast.ExprAssignDiv:
+		return &jsast.AssignExpr{Op: "/=", Left: t.transformExpr(n.Var), Right: t.transformExpr(n.Expr)}
+	case *ast.ExprAssignMod:
+		return &jsast.AssignExpr{Op: "%=", Left: t.transformExpr(n.Var), Right: t.transformExpr(n.Expr)}
+	case *ast.ExprAssignConcat:
+		return &jsast.AssignExpr{Op: "+=", Left: t.transformExpr(n.Var), Right: t.transformExpr(n.Expr)}
+	case *ast.ExprBinaryPlus:
+		return &jsast.BinaryExpr{Op: "+", Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBinaryMinus:
+		return &jsast.BinaryExpr{Op: "-", Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBinaryMul:
+		return &jsast.BinaryExpr{Op: "*", Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBinaryDiv:
+		return &jsast.BinaryExpr{Op: "/", Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBinaryMod:
+		return &jsast.BinaryExpr{Op: "%", Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBinaryConcat:
+		return &jsast.BinaryExpr{Op: "+", Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBinaryEqual:
+		return &jsast.BinaryExpr{Op: "==", Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBinaryIdentical:
+		return &jsast.BinaryExpr{Op: "===", Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBinaryNotEqual:
+		return &jsast.BinaryExpr{Op: "!=", Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBinaryNotIdentical:
+		return &jsast.BinaryExpr{Op: "!==", Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBinarySmaller:
+		return &jsast.BinaryExpr{Op: "<", Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBinarySmallerOrEqual:
+		return &jsast.BinaryExpr{Op: "<=", Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBinaryGreater:
+		return &jsast.BinaryExpr{Op: ">", Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBinaryGreaterOrEqual:
+		return &jsast.BinaryExpr{Op: ">=", Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBinaryBooleanAnd:
+		return &jsast.LogicalExpr{Op: "&&", Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBinaryBooleanOr:
+		return &jsast.LogicalExpr{Op: "||", Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBinaryLogicalAnd:
+		return &jsast.LogicalExpr{Op: "&&", Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBinaryLogicalOr:
+		return &jsast.LogicalExpr{Op: "||", Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBinaryBitwiseAnd:
+		return &jsast.BinaryExpr{Op: "&", Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBinaryBitwiseOr:
+		return &jsast.BinaryExpr{Op: "|", Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBinaryBitwiseXor:
+		return &jsast.BinaryExpr{Op: "^", Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBinaryShiftLeft:
+		return &jsast.BinaryExpr{Op: "<<", Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBinaryShiftRight:
+		return &jsast.BinaryExpr{Op: ">>", Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBinaryCoalesce:
+		return &jsast.NullCoalesceExpr{Left: t.transformExpr(n.Left), Right: t.transformExpr(n.Right)}
+	case *ast.ExprBooleanNot:
+		return &jsast.UnaryExpr{Op: "!", Operand: t.transformExpr(n.Expr), Prefix: true}
+	case *ast.ExprUnaryMinus:
+		return &jsast.UnaryExpr{Op: "-", Operand: t.transformExpr(n.Expr), Prefix: true}
+	case *ast.ExprUnaryPlus:
+		return &jsast.UnaryExpr{Op: "+", Operand: t.transformExpr(n.Expr), Prefix: true}
+	case *ast.ExprBitwiseNot:
+		return &jsast.UnaryExpr{Op: "~", Operand: t.transformExpr(n.Expr), Prefix: true}
+	case *ast.ExprPreInc:
+		return &jsast.UpdateExpr{Op: "++", Operand: t.transformExpr(n.Var), Prefix: true}
+	case *ast.ExprPreDec:
+		return &jsast.UpdateExpr{Op: "--", Operand: t.transformExpr(n.Var), Prefix: true}
+	case *ast.ExprPostInc:
+		return &jsast.UpdateExpr{Op: "++", Operand: t.transformExpr(n.Var), Prefix: false}
+	case *ast.ExprPostDec:
+		return &jsast.UpdateExpr{Op: "--", Operand: t.transformExpr(n.Var), Prefix: false}
+	case *ast.ExprFunctionCall:
+		return t.transformFunctionCall(n)
+	case *ast.ExprMethodCall:
+		return t.transformMethodCall(n)
+	case *ast.ExprStaticCall:
+		return t.transformStaticCall(n)
+	case *ast.ExprPropertyFetch:
+		return t.transformPropertyFetch(n)
+	case *ast.ExprStaticPropertyFetch:
+		return t.transformStaticPropertyFetch(n)
+	case *ast.ExprArrayDimFetch:
+		return t.transformArrayDimFetch(n)
+	case *ast.ExprArray:
+		return t.transformArray(n)
+	case *ast.ExprArrayItem:
+		return t.transformExpr(n.Val)
+	case *ast.ExprNew:
+		return t.transformNew(n)
+	case *ast.ExprTernary:
+		return t.transformTernary(n)
+	case *ast.ExprIsset:
+		return t.transformIsset(n)
+	case *ast.ExprEmpty:
+		return &jsast.CallExpr{
+			Callee: &jsast.MemberExpr{Object: &jsast.Identifier{Name: "__runtime"}, Property: &jsast.Identifier{Name: "empty"}},
+			Args:   []jsast.Expression{t.transformExpr(n.Expr)},
+		}
+	case *ast.ExprCastInt:
+		return &jsast.CallExpr{Callee: &jsast.Identifier{Name: "parseInt"}, Args: []jsast.Expression{t.transformExpr(n.Expr)}}
+	case *ast.ExprCastDouble:
+		return &jsast.CallExpr{Callee: &jsast.Identifier{Name: "parseFloat"}, Args: []jsast.Expression{t.transformExpr(n.Expr)}}
+	case *ast.ExprCastString:
+		return &jsast.CallExpr{Callee: &jsast.Identifier{Name: "String"}, Args: []jsast.Expression{t.transformExpr(n.Expr)}}
+	case *ast.ExprCastBool:
+		return &jsast.CallExpr{Callee: &jsast.Identifier{Name: "Boolean"}, Args: []jsast.Expression{t.transformExpr(n.Expr)}}
+	case *ast.ExprCastArray:
+		return &jsast.CallExpr{
+			Callee: &jsast.MemberExpr{Object: &jsast.Identifier{Name: "__runtime"}, Property: &jsast.Identifier{Name: "toArray"}},
+			Args:   []jsast.Expression{t.transformExpr(n.Expr)},
+		}
+	case *ast.ExprInstanceOf:
+		return &jsast.BinaryExpr{Op: "instanceof", Left: t.transformExpr(n.Expr), Right: t.transformExpr(n.Class)}
+	case *ast.ExprClosure:
+		return t.transformClosure(n)
+	case *ast.ExprConstFetch:
+		return t.transformConstFetch(n)
+	case *ast.ExprClassConstFetch:
+		return t.transformClassConstFetch(n)
+	case *ast.ExprInclude:
+		return t.transformInclude(n)
+	case *ast.ExprList:
+		// list() is used in assignments - handled specially
+		return &jsast.Identifier{Name: "/* list() */"}
+	case *ast.Name:
+		return &jsast.Identifier{Name: t.nameToString(n)}
+	case *ast.NameFullyQualified:
+		return &jsast.Identifier{Name: t.namePartsToString(n.Parts)}
+	default:
+		return &jsast.Identifier{Name: fmt.Sprintf("/* TODO: expr %T */", node)}
+	}
+}
+
+func (t *Transformer) transformVariable(n *ast.ExprVariable) jsast.Expression {
+	name := t.extractVarName(n)
+
+	// Check for superglobals
+	if mapped, ok := phpSuperglobals[name]; ok {
+		parts := strings.SplitN(mapped, ".", 2)
+		return &jsast.MemberExpr{
+			Object:   &jsast.Identifier{Name: parts[0]},
+			Property: &jsast.Identifier{Name: parts[1]},
+		}
+	}
+
+	// $this -> this
+	if name == "this" {
+		return &jsast.Identifier{Name: "this"}
+	}
+
+	return &jsast.Identifier{Name: sanitizeVarName(name)}
+}
+
+func (t *Transformer) transformAssign(n *ast.ExprAssign) jsast.Expression {
+	return &jsast.AssignExpr{
+		Op:    "=",
+		Left:  t.transformExpr(n.Var),
+		Right: t.transformExpr(n.Expr),
+	}
+}
+
+func (t *Transformer) transformEncapsed(n *ast.ScalarEncapsed) jsast.Expression {
+	tl := &jsast.TemplateLiteral{}
+	for _, part := range n.Parts {
+		tl.Parts = append(tl.Parts, t.transformExpr(part))
+	}
+	return tl
+}
+
+func (t *Transformer) transformFunctionCall(n *ast.ExprFunctionCall) jsast.Expression {
+	name := t.extractCallName(n.Function)
+
+	// Check if it's an async function
+	isAsync := asyncFunctions[name]
+	if isAsync {
+		t.needsAsync = true
+	}
+
+	// Map PHP built-in functions to runtime
+	callee := t.mapFunctionName(name)
+
+	var args []jsast.Expression
+	for _, a := range n.Args {
+		if arg, ok := a.(*ast.Argument); ok {
+			args = append(args, t.transformExpr(arg.Expr))
+		}
+	}
+
+	return &jsast.CallExpr{Callee: callee, Args: args, Await: isAsync}
+}
+
+func (t *Transformer) transformMethodCall(n *ast.ExprMethodCall) jsast.Expression {
+	obj := t.transformExpr(n.Var)
+	method := t.extractName(n.Method)
+
+	var args []jsast.Expression
+	for _, a := range n.Args {
+		if arg, ok := a.(*ast.Argument); ok {
+			args = append(args, t.transformExpr(arg.Expr))
+		}
+	}
+
+	return &jsast.CallExpr{
+		Callee: &jsast.MemberExpr{Object: obj, Property: &jsast.Identifier{Name: method}},
+		Args:   args,
+	}
+}
+
+func (t *Transformer) transformStaticCall(n *ast.ExprStaticCall) jsast.Expression {
+	className := t.extractCallName(n.Class)
+	method := t.extractName(n.Call)
+
+	var args []jsast.Expression
+	for _, a := range n.Args {
+		if arg, ok := a.(*ast.Argument); ok {
+			args = append(args, t.transformExpr(arg.Expr))
+		}
+	}
+
+	return &jsast.CallExpr{
+		Callee: &jsast.MemberExpr{
+			Object:   &jsast.Identifier{Name: className},
+			Property: &jsast.Identifier{Name: method},
+		},
+		Args: args,
+	}
+}
+
+func (t *Transformer) transformPropertyFetch(n *ast.ExprPropertyFetch) jsast.Expression {
+	return &jsast.MemberExpr{
+		Object:   t.transformExpr(n.Var),
+		Property: &jsast.Identifier{Name: t.extractName(n.Prop)},
+	}
+}
+
+func (t *Transformer) transformStaticPropertyFetch(n *ast.ExprStaticPropertyFetch) jsast.Expression {
+	return &jsast.MemberExpr{
+		Object:   t.transformExpr(n.Class),
+		Property: &jsast.Identifier{Name: t.extractVarName(n.Prop)},
+	}
+}
+
+func (t *Transformer) transformArrayDimFetch(n *ast.ExprArrayDimFetch) jsast.Expression {
+	obj := t.transformExpr(n.Var)
+	if n.Dim == nil {
+		// $arr[] = val -> array push, handled at assignment level
+		return obj
+	}
+	return &jsast.MemberExpr{
+		Object:   obj,
+		Property: t.transformExpr(n.Dim),
+		Computed: true,
+	}
+}
+
+func (t *Transformer) transformArray(n *ast.ExprArray) jsast.Expression {
+	hasStringKeys := false
+	for _, item := range n.Items {
+		if ai, ok := item.(*ast.ExprArrayItem); ok && ai.Key != nil {
+			hasStringKeys = true
+			break
+		}
+	}
+
+	if hasStringKeys {
+		// Associative array -> PhpArray or object
+		obj := &jsast.ObjectExpr{}
+		for _, item := range n.Items {
+			if ai, ok := item.(*ast.ExprArrayItem); ok {
+				prop := &jsast.ObjectProperty{Value: t.transformExpr(ai.Val)}
+				if ai.Key != nil {
+					prop.Key = t.transformExpr(ai.Key)
+					prop.Computed = true
+				}
+				obj.Properties = append(obj.Properties, prop)
+			}
+		}
+		return &jsast.CallExpr{
+			Callee: &jsast.MemberExpr{Object: &jsast.Identifier{Name: "__runtime"}, Property: &jsast.Identifier{Name: "array"}},
+			Args:   []jsast.Expression{obj},
+		}
+	}
+
+	// Indexed array -> array literal
+	arr := &jsast.ArrayExpr{}
+	for _, item := range n.Items {
+		if ai, ok := item.(*ast.ExprArrayItem); ok {
+			arr.Elements = append(arr.Elements, t.transformExpr(ai.Val))
+		}
+	}
+	return arr
+}
+
+func (t *Transformer) transformNew(n *ast.ExprNew) jsast.Expression {
+	className := t.extractCallName(n.Class)
+	var args []jsast.Expression
+	for _, a := range n.Args {
+		if arg, ok := a.(*ast.Argument); ok {
+			args = append(args, t.transformExpr(arg.Expr))
+		}
+	}
+	return &jsast.NewExpr{Callee: &jsast.Identifier{Name: className}, Args: args}
+}
+
+func (t *Transformer) transformTernary(n *ast.ExprTernary) jsast.Expression {
+	cond := t.transformExpr(n.Cond)
+	if n.IfTrue == nil {
+		// Short ternary: $a ?: $b -> a || b
+		return &jsast.LogicalExpr{Op: "||", Left: cond, Right: t.transformExpr(n.IfFalse)}
+	}
+	return &jsast.ConditionalExpr{
+		Test: cond, Consequent: t.transformExpr(n.IfTrue), Alternate: t.transformExpr(n.IfFalse),
+	}
+}
+
+func (t *Transformer) transformIsset(n *ast.ExprIsset) jsast.Expression {
+	if len(n.Vars) == 1 {
+		return &jsast.CallExpr{
+			Callee: &jsast.MemberExpr{Object: &jsast.Identifier{Name: "__runtime"}, Property: &jsast.Identifier{Name: "isset"}},
+			Args:   []jsast.Expression{t.transformExpr(n.Vars[0])},
+		}
+	}
+	// Multiple vars: isset($a, $b) -> __runtime.isset(a) && __runtime.isset(b)
+	var exprs jsast.Expression
+	for i, v := range n.Vars {
+		call := &jsast.CallExpr{
+			Callee: &jsast.MemberExpr{Object: &jsast.Identifier{Name: "__runtime"}, Property: &jsast.Identifier{Name: "isset"}},
+			Args:   []jsast.Expression{t.transformExpr(v)},
+		}
+		if i == 0 {
+			exprs = call
+		} else {
+			exprs = &jsast.LogicalExpr{Op: "&&", Left: exprs, Right: call}
+		}
+	}
+	return exprs
+}
+
+func (t *Transformer) transformClosure(n *ast.ExprClosure) jsast.Expression {
+	arrow := &jsast.ArrowFunc{IsAsync: false}
+	for _, p := range n.Params {
+		arrow.Params = append(arrow.Params, t.transformParam(p))
+	}
+	for _, s := range n.Stmts {
+		arrow.Body = append(arrow.Body, t.transformStmt(s)...)
+	}
+	return arrow
+}
+
+func (t *Transformer) transformConstFetch(n *ast.ExprConstFetch) jsast.Expression {
+	name := t.extractCallName(n.Const)
+	switch strings.ToLower(name) {
+	case "true":
+		return &jsast.Literal{Value: "true", Kind: "bool"}
+	case "false":
+		return &jsast.Literal{Value: "false", Kind: "bool"}
+	case "null":
+		return &jsast.Literal{Value: "null", Kind: "null"}
+	case "php_eol":
+		return &jsast.Literal{Value: `"\\n"`, Kind: "string"}
+	case "directory_separator", "dir_separator":
+		return &jsast.Literal{Value: `"/"`, Kind: "string"}
+	default:
+		return &jsast.MemberExpr{
+			Object:   &jsast.Identifier{Name: "__runtime"},
+			Property: &jsast.Identifier{Name: "CONST_" + name},
+		}
+	}
+}
+
+func (t *Transformer) transformClassConstFetch(n *ast.ExprClassConstFetch) jsast.Expression {
+	className := t.extractCallName(n.Class)
+	constName := t.extractName(n.Const)
+	if className == "self" && t.inClass != "" {
+		className = t.inClass
+	}
+	return &jsast.MemberExpr{
+		Object:   &jsast.Identifier{Name: className},
+		Property: &jsast.Identifier{Name: constName},
+	}
+}
+
+func (t *Transformer) transformInclude(n *ast.ExprInclude) jsast.Expression {
+	// include/require -> dynamic import (simplified)
+	return &jsast.CallExpr{
+		Callee: &jsast.MemberExpr{Object: &jsast.Identifier{Name: "__runtime"}, Property: &jsast.Identifier{Name: "include"}},
+		Args:   []jsast.Expression{t.transformExpr(n.Expr)},
+		Await:  true,
+	}
+}
---
+++ internal/transformer/helpers.go
@@ -0,0 +1,208 @@
+package transformer
+
+import (
+	"strings"
+
+	"github.com/VKCOM/php-parser/pkg/ast"
+	"github.com/kmmz532/php2js/internal/jsast"
+)
+
+// extractVarName gets the variable name from a variable node.
+func (t *Transformer) extractVarName(node ast.Vertex) string {
+	switch n := node.(type) {
+	case *ast.ExprVariable:
+		if id, ok := n.Name.(*ast.Identifier); ok {
+			return sanitizeVarName(string(id.Value))
+		}
+	case *ast.Identifier:
+		return sanitizeVarName(string(n.Value))
+	}
+	return "unknown"
+}
+
+// extractName gets a name from an identifier node.
+func (t *Transformer) extractName(node ast.Vertex) string {
+	switch n := node.(type) {
+	case *ast.Identifier:
+		return string(n.Value)
+	case *ast.Name:
+		return t.nameToString(n)
+	case *ast.NameFullyQualified:
+		return t.namePartsToString(n.Parts)
+	}
+	return "unknown"
+}
+
+// extractCallName extracts the function/class name from a call target.
+func (t *Transformer) extractCallName(node ast.Vertex) string {
+	switch n := node.(type) {
+	case *ast.Name:
+		return t.nameToString(n)
+	case *ast.NameFullyQualified:
+		return t.namePartsToString(n.Parts)
+	case *ast.Identifier:
+		return string(n.Value)
+	case *ast.ExprVariable:
+		return t.extractVarName(n)
+	}
+	return "unknown"
+}
+
+// nameToString converts a Name node to a string.
+func (t *Transformer) nameToString(n *ast.Name) string {
+	return t.namePartsToString(n.Parts)
+}
+
+// namePartsToString joins name parts into a string.
+func (t *Transformer) namePartsToString(parts []ast.Vertex) string {
+	var names []string
+	for _, p := range parts {
+		if np, ok := p.(*ast.NamePart); ok {
+			names = append(names, string(np.Value))
+		}
+	}
+	return strings.Join(names, "_")
+}
+
+// getModifiers extracts access modifier string.
+func (t *Transformer) getModifiers(mods []ast.Vertex) string {
+	for _, m := range mods {
+		if id, ok := m.(*ast.Identifier); ok {
+			v := string(id.Value)
+			if v == "private" || v == "protected" || v == "public" || v == "static" {
+				return v
+			}
+		}
+	}
+	return "public"
+}
+
+// sanitizeVarName makes a PHP variable name safe for JS.
+func sanitizeVarName(name string) string {
+	// Remove $ prefix if present
+	name = strings.TrimPrefix(name, "$")
+	if jsReservedWords[name] {
+		return name + "_"
+	}
+	// Replace invalid chars
+	name = strings.ReplaceAll(name, "-", "_")
+	return name
+}
+
+// escapeJSString escapes a string for JS.
+func escapeJSString(s string) string {
+	s = strings.ReplaceAll(s, "\\", "\\\\")
+	s = strings.ReplaceAll(s, "`", "\\`")
+	s = strings.ReplaceAll(s, "$", "\\$")
+	s = strings.ReplaceAll(s, "\n", "\\n")
+	s = strings.ReplaceAll(s, "\r", "\\r")
+	s = strings.ReplaceAll(s, "\t", "\\t")
+	return s
+}
+
+// mapFunctionName maps PHP function names to JS runtime equivalents.
+func (t *Transformer) mapFunctionName(name string) jsast.Expression {
+	// PHP built-in functions -> runtime
+	builtins := map[string]string{
+		// String functions
+		"strlen": "strlen", "strpos": "strpos", "strrpos": "strrpos",
+		"substr": "substr", "str_replace": "str_replace",
+		"explode": "explode", "implode": "implode",
+		"trim": "trim", "ltrim": "ltrim", "rtrim": "rtrim",
+		"strtolower": "strtolower", "strtoupper": "strtoupper",
+		"ucfirst": "ucfirst", "lcfirst": "lcfirst",
+		"sprintf": "sprintf", "printf": "printf",
+		"htmlspecialchars": "htmlspecialchars", "htmlspecialchars_decode": "htmlspecialchars_decode",
+		"htmlentities": "htmlentities", "strip_tags": "strip_tags",
+		"nl2br": "nl2br", "wordwrap": "wordwrap",
+		"str_pad": "str_pad", "str_repeat": "str_repeat",
+		"str_split": "str_split", "chunk_split": "chunk_split",
+		"ord": "ord", "chr": "chr",
+		"mb_strlen": "mb_strlen", "mb_strpos": "mb_strpos",
+		"mb_substr": "mb_substr", "mb_strtolower": "mb_strtolower",
+		"mb_strtoupper": "mb_strtoupper", "mb_convert_encoding": "mb_convert_encoding",
+		"mb_detect_encoding": "mb_detect_encoding", "mb_internal_encoding": "mb_internal_encoding",
+		// Array functions
+		"count": "count", "sizeof": "count",
+		"array_push": "array_push", "array_pop": "array_pop",
+		"array_shift": "array_shift", "array_unshift": "array_unshift",
+		"array_merge": "array_merge", "array_slice": "array_slice",
+		"array_splice": "array_splice",
+		"array_keys": "array_keys", "array_values": "array_values",
+		"array_flip": "array_flip", "array_reverse": "array_reverse",
+		"array_map": "array_map", "array_filter": "array_filter",
+		"array_reduce": "array_reduce", "array_walk": "array_walk",
+		"array_search": "array_search", "in_array": "in_array",
+		"array_key_exists": "array_key_exists", "array_unique": "array_unique",
+		"sort": "sort", "rsort": "rsort", "asort": "asort",
+		"arsort": "arsort", "ksort": "ksort", "krsort": "krsort",
+		"usort": "usort", "uksort": "uksort", "uasort": "uasort",
+		"array_diff": "array_diff", "array_intersect": "array_intersect",
+		"range": "range", "compact": "compact", "extract": "extract",
+		"list": "list",
+		// Type functions
+		"is_array": "is_array", "is_string": "is_string",
+		"is_int": "is_int", "is_integer": "is_int",
+		"is_float": "is_float", "is_double": "is_float",
+		"is_bool": "is_bool", "is_null": "is_null",
+		"is_numeric": "is_numeric", "is_object": "is_object",
+		"gettype": "gettype", "settype": "settype",
+		"intval": "intval", "floatval": "floatval", "strval": "strval",
+		// Math
+		"abs": "abs", "ceil": "ceil", "floor": "floor", "round": "round",
+		"max": "max", "min": "min", "pow": "pow", "sqrt": "sqrt",
+		"rand": "rand", "mt_rand": "mt_rand",
+		// Hash/Crypto
+		"md5": "md5", "sha1": "sha1", "hash": "hash",
+		"crc32": "crc32", "base64_encode": "base64_encode",
+		"base64_decode": "base64_decode",
+		// JSON
+		"json_encode": "json_encode", "json_decode": "json_decode",
+		// URL
+		"urlencode": "urlencode", "urldecode": "urldecode",
+		"rawurlencode": "rawurlencode", "rawurldecode": "rawurldecode",
+		"http_build_query": "http_build_query", "parse_url": "parse_url",
+		"parse_str": "parse_str",
+		// Date/Time
+		"date": "date", "time": "time", "mktime": "mktime",
+		"strtotime": "strtotime", "microtime": "microtime",
+		"gmdate": "gmdate",
+		// File I/O -> R2
+		"file_get_contents": "file_get_contents", "file_put_contents": "file_put_contents",
+		"file_exists": "file_exists", "unlink": "unlink", "rename": "rename",
+		"fopen": "fopen", "fclose": "fclose", "fread": "fread",
+		"fwrite": "fwrite", "fgets": "fgets", "feof": "feof",
+		"scandir": "scandir", "glob": "glob",
+		"mkdir": "mkdir", "rmdir": "rmdir", "is_dir": "is_dir", "is_file": "is_file",
+		"dirname": "dirname", "basename": "basename", "pathinfo": "pathinfo",
+		"realpath": "realpath", "file": "file",
+		// Output
+		"echo": "echo", "print": "echo", "var_dump": "var_dump",
+		"print_r": "print_r", "var_export": "var_export",
+		// Regex
+		"preg_match": "preg_match", "preg_match_all": "preg_match_all",
+		"preg_replace": "preg_replace", "preg_split": "preg_split",
+		"preg_replace_callback": "preg_replace_callback",
+		// HTTP
+		"header": "header", "setcookie": "setcookie",
+		"session_start": "session_start", "session_destroy": "session_destroy",
+		"session_id": "session_id",
+		// Misc
+		"isset": "isset", "unset": "unset", "empty": "empty",
+		"die": "die", "exit": "exit",
+		"defined": "defined", "define": "define",
+		"class_exists": "class_exists", "function_exists": "function_exists",
+		"call_user_func": "call_user_func", "call_user_func_array": "call_user_func_array",
+		"sleep": "sleep", "usleep": "usleep",
+	}
+
+	if jsName, ok := builtins[name]; ok {
+		return &jsast.MemberExpr{
+			Object:   &jsast.Identifier{Name: "__runtime"},
+			Property: &jsast.Identifier{Name: jsName},
+		}
+	}
+
+	// User-defined function - use as-is
+	return &jsast.Identifier{Name: name}
+}
